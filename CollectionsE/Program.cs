d9b5fb3525a618482a9e0e59e8a7384789a67141
Wellington <wellingtoncardoso1514@gmail.com>
2026-04-08 17:38:43 -0300

    Adicionar arquivos de projeto.

---
+++ CollectionsE/Program.cs
@@ -0,0 +1,200 @@
+﻿/*
+    Seja um aplicativo de gerenciamento de músicas onde os usuários
+podem organizar suas faixas favoritas em playlists personalizadas. 
+Para cada playlist, é essencial que o usuário tenha controle total sobre a 
+sequência de reprodução das músicas, permitindo reordená-las livremente a qualquer momento.
+Além disso, o aplicativo precisa oferecer a funcionalidade de reprodução aleatória para uma playlist específica,
+proporcionando uma experiência de audição dinâmica e variada, sem, contudo, alterar a ordem original que o usuário definiu.
+O desafio é criar uma estrutura robusta que suporte a adição e remoção eficiente de músicas,
+a reordenação flexível dentro das playlists e a seleção de faixas tanto em modo sequencial quanto aleatório.
+
+    Funções que vamos implementar:
+    //     [x] Criar as classes para musicas e playlist
+    //     [x] Listar músicas da playlist
+    //     [x] Adicionar música à playlist
+    //     [x] Obter uma música específica da playlist
+    //     [x] Remover música da playlist
+    //     [x] Tocar músicas da playlist em modo aleatório 
+    //     [ ] Reordenar músicas segundo alguma lógica específica (ex. duração)
+    //     [ ] Uma playlist não pode ter músicas repetidas
+    //     [ ] Exibir as 10 músicas mais tocadas em todas as playlists (ranking)
+    //     [ ] Player de música com:
+    //     [ ] - Fila de reprodução (para músicas avulsas e/ou playlists)
+    //     [ ] - Histórico de reprodução
+ 
+*/
+
+using System.Collections;
+using System.ComponentModel;
+using System.Globalization;
+using System.Net.WebSockets;
+
+var musica1 = new Musica { Titulo = "Que País é esse?", Artista = "Legião Urbana", Duracao = 350 };
+
+var musica2 = new Musica { Titulo = "Faroeste Caboclo", Artista = "Legião Urbana", Duracao = 565 };
+
+var musica3 = new Musica { Titulo = "Tempo Perdido", Artista = "Legião Urbana", Duracao = 300 };
+
+var musica4 = new Musica { Titulo = "Índios", Artista = "Legião Urbana", Duracao = 323 };
+
+var rockNacional = new Playlist { Nome = "Rock Nacional" };
+
+
+rockNacional.Add(musica1);
+
+rockNacional.Add(musica2);
+
+rockNacional.Add(musica3);
+
+rockNacional.Add(musica4);
+
+ExibirPlaylist(rockNacional);
+
+
+RemoverMusicaPeloTitulo(rockNacional, "Que País é esse?");
+
+
+ExibirMusicaAleatoria(rockNacional);
+
+
+rockNacional.OrdenarPorDuracao();
+
+ExibirPlaylist(rockNacional);
+
+void ExibirPlaylist(Playlist playlist)
+{
+    Console.WriteLine($"\n Tocando as músicas de {playlist.Nome}");
+
+    foreach (var musica in playlist)
+    {
+        Console.WriteLine($"\t - {musica.Titulo} - {musica.Artista} - {musica.Duracao} segundos");
+    }
+}
+
+void RemoverMusicaPeloTitulo(Playlist playlist, string titulo)
+{
+    var musicaEncontrada = playlist.ObterPeloTitulo(titulo);
+
+    if (musicaEncontrada is not null)
+    {
+        Console.WriteLine("\nRemovendo músic...");
+        rockNacional.Remove(musicaEncontrada);
+    }
+    else
+    {
+        Console.WriteLine("\nMúsica não encontrada");
+    }
+
+    ExibirPlaylist(rockNacional);
+}
+
+
+void ExibirMusicaAleatoria(Playlist playlist)
+{
+    var musicaAleatoria = playlist.ObterAleatoria();
+
+    if (musicaAleatoria is not null)
+    {
+        Console.WriteLine($"\nA música aleatória escolhida é: {musicaAleatoria.Titulo}");
+    }
+    else
+    {
+        Console.WriteLine("Nenhuma música foi encontrada!");
+    }
+}
+
+
+public class Musica : IComparable
+{
+    public string Titulo { get; set; } = string.Empty;
+    public string Artista { get; set; } = string.Empty;
+    public int Duracao { get; set; }
+
+    public int CompareTo(object? outraMusica)
+    {
+        if (outraMusica is null) return -1;
+
+        if (outraMusica is Musica musica)
+        {
+            return this.Duracao.CompareTo(musica.Duracao);
+        }
+
+        return 0;
+    }
+}
+
+public class Playlist : ICollection<Musica>
+{
+    private List<Musica> lista = [];
+    public string Nome { get; set; } = string.Empty;
+
+    public int Count => lista.Count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(Musica item)
+    {
+        lista.Add(item);
+    }
+
+    public void Clear()
+    {
+        lista.Clear();
+    }
+
+    public bool Contains(Musica item)
+    {
+        return lista.Contains(item);
+    }
+
+    public void CopyTo(Musica[] array, int arrayIndex)
+    {
+        lista.CopyTo(array, arrayIndex);
+    }
+
+
+    public Musica? ObterPeloTitulo(string nome)
+    {
+        foreach (var musica in lista)
+        {
+            if (musica.Titulo == nome) return musica;
+        }
+
+        return null;
+    }
+
+
+    public Musica? ObterAleatoria()
+    {
+        var random = new Random();
+
+        if (Count == 0) return null;
+
+        var indiceAleatorio = random.Next(0, Count - 1);
+
+        return lista[indiceAleatorio];
+    }
+
+
+    public void OrdenarPorDuracao()
+    {
+        //lista.Sort((a, b) => a.Duracao.CompareTo(b.Duracao));
+
+        lista.Sort();
+    }
+
+    public IEnumerator<Musica> GetEnumerator()
+    {
+        return lista.GetEnumerator();
+    }
+
+    public bool Remove(Musica item)
+    {
+        return lista.Remove(item);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
