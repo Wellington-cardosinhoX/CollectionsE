148cec5a021f8f22286dc555efdb19ed5c2aff10
wellington.ccamilo <wellington.ccamilo@CAS0718261W11-1>
2026-04-10 21:24:46 -0300

    Revert "ererer"
    This reverts commit aff6faf75c4e4f390d3bcb39e1ea3b7a82ec098d.

--- CollectionsE/Program.cs
+++ CollectionsE/Program.cs
@@ -143,8 +143,6 @@ public class SecondPlaylist : ICollection<SecondMusics>
     private List<SecondMusics> lista = [];
 
     public string Nome { get; set; } = string.Empty;
-
-    public Count => lista.Count;
 }
 
 
